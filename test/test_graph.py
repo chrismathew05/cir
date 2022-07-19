34456cea0cb365aaca2a3371128ee42cd18e6774
Chris Mathew <chrismathew05@gmail.com>
2022-07-19 17:47:06 -0400

    Added basic test case

--- test/test_graph.py
+++ test/test_graph.py
@@ -9,10 +9,24 @@ logger = logging.getLogger(__name__)
 class TestGraph(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        logger.info("\n=== TESTING GRAPH.PY ===")
+        """Sets up circuit graph instances for testing"""
 
-    def test_graph(self) -> None:
-        pass
+        logger.info("\n=== TESTING GRAPH.PY ===")
+        cls.A = CircuitGraph("app/circuits/test.xml")
+
+    def test_find_cycles(self) -> None:
+        """Test ability to find cycles"""
+
+        cycles = self.A.find_cycles()
+        self.assertEqual(cycles, [])
+
+    def test_find_paths(self) -> None:
+        """Test ability to find paths"""
+
+        paths = self.A.find_paths("board_0_wire_0", "board_0_wire_1")
+        self.assertEqual(
+            paths, [["board_0_wire_0", "board_0_wire_2", "board_0_wire_1"]]
+        )
 
 
 if __name__ == "__main__":
